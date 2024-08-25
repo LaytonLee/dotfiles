bc8faf6d581f9d9e8256516491cc0fb4dd00ef65
layton <cheng_l@hotmail.com>
2024-08-25 16:05:00 +0800

    ZSH: Add alias

---
+++ zsh/.zshrc.d/alias.zsh
@@ -0,0 +1,5 @@
+# quickly open the browser from terminal
+alias chrome="/usr/bin/open -a '/Applications/Google Chrome.app'"
+alias brave="/usr/bin/open -a '/Applications/Brave Browser.app'"
+alias firefox="/usr/bin/open -a '/Applications/FireFox.app'"
+alias tor="/usr/bin/open -a '/Applications/Tor Browser.app'"
