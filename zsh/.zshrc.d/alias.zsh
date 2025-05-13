df48d707d8585a6970b78fec0f5edd8390314895
layton <cheng_l@hotmail.com>
2025-05-13 04:03:52 -0400

    add JD-GUI alias

--- zsh/.zshrc.d/alias.zsh
+++ zsh/.zshrc.d/alias.zsh
@@ -3,3 +3,6 @@ alias chrome="/usr/bin/open -a '/Applications/Google Chrome.app'"
 alias brave="/usr/bin/open -a '/Applications/Brave Browser.app'"
 alias firefox="/usr/bin/open -a '/Applications/FireFox.app'"
 alias tor="/usr/bin/open -a '/Applications/Tor Browser.app'"
+
+# jd-gui
+[ -f /Applications/JD-GUI.app ] && alias jdgui="java -jar /Applications/JD-GUI.app/Contents/Resources/Java/jd-gui-1.6.6-min.jar"
